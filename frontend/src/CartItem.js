0010466668fca211de9798098044a9cf8e127899
ayhliu <ayhliu@live.unc.edu>
2021-07-09 19:08:27 -0400

    Added item's delivery time dropdown list (not implemented yet)

--- frontend/src/CartItem.js
+++ frontend/src/CartItem.js
@@ -5,6 +5,7 @@ function CartItem({ itemId, qty: quantity, refreshCart, ...misc }) {
   const [qty, setQty] = useState(quantity);
   const [item, setItem] = useState();
   const [isLoggedIn, setIsLoggedIn] = useState(true); //assume logged in already.
+  let dTime = "Saturday";
   const deleteItem = () => {
     Axios.delete(process.env.REACT_APP_BACKEND_API + "/cart/" + itemId, {
       withCredentials: true,
@@ -36,6 +37,10 @@ function CartItem({ itemId, qty: quantity, refreshCart, ...misc }) {
   };
   const handleQty = (e) => {
     setQty(e.target.value);
+    updateCart(e.target.value);
+  };
+  const handleDTime = (e) => {
+    dTime = e.target.value;
     updateCart(e.target.value);
   };
   useEffect(() => {
@@ -78,6 +83,12 @@ function CartItem({ itemId, qty: quantity, refreshCart, ...misc }) {
                   })}
                 </select>
               </div>
+            </div>
+            <div className="select">
+              <select onChange={handleDTime} value={dTime}>
+                <option> Saturday, July 10 </option>
+                <option> Sunday, July 11 </option>
+              </select>
             </div>
           </div>
         </div>
